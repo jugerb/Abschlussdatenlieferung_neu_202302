38e13f7f9a6dbc3d44e28c0a5217710f8c4c83c6
jjjellyfish <jgerb@protonmail.com>
2023-02-16 11:42:49 +0100

    add 2020 (Datensatz vorheriger Datenlieferung halbieren)

--- R_Skripte/01_data_load.R
+++ R_Skripte/01_data_load.R
@@ -1,6 +1,8 @@
 ### Data load & save
 # Skript lädt Tabellen, benennt diese um und speichert sie als .rda-Datensätze
 
+library(tidyverse)
+library(lubridate)
 
 #### 2019 ####
 # data load
@@ -37,7 +39,70 @@ save(kht12,file="kht12.rda")
 
 
 #### 2020 ####
-#to do
+setwd('O:/U5279/Routinedaten/Abschlussdatenlieferung20_21/Daten/02_roh')
+
+stamm01 <-  read.csv("01_stamm_20-21_2022-07-20 18-55-30.csv", sep=",")
+tg02 <-  read.csv("02_tg2_20_21_08-04 17-31-03.csv", sep=",")
+vz03 <-  read.csv("03_vz_20-21_2022-07-20 18-57-44.csv", sep=",")
+vzpg04 <-  read.csv("04_vz_pg_20-21_2022-07-20 19-01-26.csv", sep=",")
+pl05 <-  read.csv("05_pflegeleistungen_20-21_2022-07-21 09-25-08.csv", sep=",")
+plart06 <-  read.csv("06_art_pg_20_21_2212202022-12-20 15-53-22.csv", sep=",")
+pg07 <-  read.csv("07_pflegegeld_20_21_220722.csv", sep=",")
+hkp08 <-  read.csv("08_hkp_20-21_2022-07-21 14-31-31.csv", sep=",")
+hkpgop09 <-  read.csv("09_gop_hkp_20-21_2022-07-21 15-25-41.csv", sep=",")
+sgbv10 <-  read.csv("10_sgbv_20-21_2022-07-21 15-31-46.csv", sep=",")
+kh11 <-  read.csv("11_kh_20-21_2022-07-21 15-37-30.csv", sep=",")
+kht12 <-  read.csv("12_kht_20-21_2022-07-21 15-41-21.csv", sep=",")
+
+
+# data save
+setwd('O:/U5279/Routinedaten/Abschlussdatenlieferung_neu_202302/Daten/02_rdata/2020')
+
+# nur 2020 übrig behalten
+stamm01 <- stamm01 %>% filter(V_STERBEDATUM == '' | ymd(V_STERBEDATUM) <= as.Date('2020-12-31'))
+save(stamm01, file="stamm01.rda")
+
+save(tg02, file="tg02.rda")
+
+# nur 2020 übrig behalten
+vz03 <- vz03 %>% filter(ymd(V_VZ_VON) <= as.Date('2020-12-31'))
+save(vz03, file="vz03.rda")
+
+# nur 2020 übrig behalten
+vzpg04 <- vzpg04 %>% filter(ymd(V_PG_VON) <= as.Date('2020-12-31'))
+save(vzpg04, file="vzpg04.rda")
+
+# PL und PL-Art gemeinsam betrachten
+# PL-Art nur 2020 übrig behalten
+plart06 <- plart06 %>% filter(ymd(LEISTUNG_VON) <= as.Date('2020-12-31'))
+pl05 <- pl05 %>% filter(PL_RENR %in% plart06$PL_RENR)
+
+save(pl05, file="pl05.rda")
+save(plart06, file="plart06.rda")
+
+# nur 2020 übrig behalten
+pg07 <- pg07 %>% filter(ymd(LEISTUNG_VON) <= as.Date('2020-12-31'))
+save(pg07, file="pg07.rda")
+
+# nur 2020 übrig behalten
+hkp08 <- hkp08 %>% filter(ymd(HKP_DATUM) <= as.Date('2020-12-31'))
+save(hkp08, file="hkp08.rda")
+
+# nur 2020 übrig behalten
+hkpgop09 <- hkpgop09 %>% filter(HKP_FALLNR %in% hkp08$HKP_FALLNR)
+save(hkpgop09, file="hkpgop09.rda")
+
+# nur 2020 übrig behalten
+sgbv10 <- sgbv10 %>% filter(ymd(HM_DATUM) <= as.Date('2020-12-31'))
+save(sgbv10, file="sgbv10.rda")
+
+# nur 2020 übrig behalten
+kh11 <- kh11 %>% filter(ymd(KH_BEGINN_DAT) <= as.Date('2020-12-31'))
+save(kh11, file="kh11.rda")
+
+# nur 2020 übrig behalten
+kht12 <- kht12 %>% filter(ymd(KT_DATUM) <= as.Date('2020-12-31'))
+save(kht12, file="kht12.rda")
 
 
 #### 2021 ####
