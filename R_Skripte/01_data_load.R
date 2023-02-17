27bcbad2d9009e76839bc756c907db6f06a87432
jjjellyfish <jgerb@protonmail.com>
2023-02-17 12:45:04 +0100

    Korrektur Datensatz HKP-Gops, Differenzierung hkpgop & hkpmedi

--- R_Skripte/01_data_load.R
+++ R_Skripte/01_data_load.R
@@ -16,10 +16,19 @@ pl05 <-  read.csv("05_pflegeleistungen_2019_2023-02-10 14-46-14.csv", sep=",")
 plart06 <-  read.csv("06_art_pg_2019_2023-02-10 15-03-57.csv", sep=",")
 pg07 <-  read.csv("07_pflegegeld_2019_2023-02-10 15-39-17.csv", sep=",")
 hkp08 <-  read.csv("08_hkp_2019_2023-02-10 15-55-03.csv", sep=",")
-hkpgop09 <-  read.csv("09_gop_hkp_2019_2023-02-10 16-41-30.csv", sep=",")
+hkpgop09 <-  read.csv("09_hkp_gop_2019_2023-02-16 17-12-49.csv", sep=",")
+hkpmedi09 <-  read.csv("09_gop_hkp_2019_2023-02-10 16-41-30.csv", sep=",")
 sgbv10 <-  read.csv("10_sgbv_2019_2023-02-10 16-43-34.csv", sep=",")
 kh11 <-  read.csv("11_KH_2019_2023-02-10 16-49-50.csv", sep=",")
 kht12 <-  read.csv("12_kht_2019_2023-02-10 16-51-52.csv", sep=",")
+
+# #hkpgops
+# sum(hkpgop09$HKP_FALLNR %in% hkp08$HKP_FALLNR)
+# sum(hkp08$HKP_FALLNR %in% hkpgop09$HKP_FALLNR)
+# 
+# #hkpmedis
+# sum(hkpmedi09$HKP_FALLNR %in% hkp08$HKP_FALLNR)
+# sum(hkp08$HKP_FALLNR %in% hkpmedi09$HKP_FALLNR)
 
 # data save
 setwd('O:/U5279/Routinedaten/Abschlussdatenlieferung_neu_202302/Daten/02_rdata/2019')
@@ -33,6 +42,7 @@ save(plart06,file="plart06.rda")
 save(pg07,file="pg07.rda")
 save(hkp08,file="hkp08.rda")
 save(hkpgop09,file="hkpgop09.rda")
+save(hkpmedi09,file="hkpmedi09.rda")
 save(sgbv10,file="sgbv10.rda")
 save(kh11,file="kh11.rda")
 save(kht12,file="kht12.rda")
@@ -116,10 +126,19 @@ pl05 <-  read.csv("05_pflegeleistungen_2021_2023-02-14 15-47-03.csv", sep=",")
 plart06 <-  read.csv("06_art_pg_2021_2023-02-14 16-35-34.csv", sep=",")
 pg07 <-  read.csv("07_pflegegeld_2021_2023-02-14 18-02-39.csv", sep=",")
 hkp08 <-  read.csv("08_hkp_2021_2023-02-14 19-36-18.csv", sep=",")
-hkpgop09 <-  read.csv("09_gop_hkp_2021_2023-02-14 18-36-37.csv", sep=",")
+hkpgop09 <-  read.csv("09_hkp_gop_2021_2023-02-16 17-22-30.csv", sep=",")
+hkpmedi09 <-  read.csv("09_gop_hkp_2021_2023-02-14 18-36-37.csv", sep=",")
 sgbv10 <-  read.csv("10_sgbv_2021_2023-02-14 18-39-15.csv", sep=",")
 kh11 <-  read.csv("11_kh_2021_2023-02-14 18-55-36.csv", sep=",")
 kht12 <-  read.csv("12_kht_2021_2023-02-14 19-05-51.csv", sep=",")
+
+# #hkpgops
+# sum(hkpgop09$HKP_FALLNR %in% hkp08$HKP_FALLNR)
+# sum(hkp08$HKP_FALLNR %in% hkpgop09$HKP_FALLNR)
+# 
+# #hkpmedis
+# sum(hkpmedi09$HKP_FALLNR %in% hkp08$HKP_FALLNR)
+# sum(hkp08$HKP_FALLNR %in% hkpmedi09$HKP_FALLNR)
 
 # data save
 setwd('O:/U5279/Routinedaten/Abschlussdatenlieferung_neu_202302/Daten/02_rdata/2021')
